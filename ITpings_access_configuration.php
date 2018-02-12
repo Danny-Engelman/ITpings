5c2b8d3dba212d19501bd4456aa10032bf7b5e2b
Danny Engelman <danny@engelman.nl>
2018-02-12 14:30:00 +0100

    Create ITpings_access_configuration.php

---
+++ ITpings_access_configuration.php
@@ -0,0 +1,10 @@
+<?php
+/* WARNING! Change to a value of your own choice! secret key gives admin access */
+define('SECRETKEY', 'EDITME');      // required in the HTTP integration to accept only your requests
+
+/* Database access settings, if you don't know what to use, contact your Webserver Administrator */
+define('DBHOST', 'localhost');      // Typically the Web and Database server run on the same server: localhost
+define('DBNAME', 'ITpings');        // The name given by your Database Administrator
+define('DBUSER', 'YOURUSERNAME');
+define('DBPASSWORD', 'YOURPASSWORD');
+?>
